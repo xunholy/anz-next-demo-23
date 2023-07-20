fac1f4757d0c0845a8e03cf9303a9524b4585c09
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2023-07-20 21:13:19 +1000

    feat: add hack for using configconnector with WLI
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

---
+++ hack/bootstrap.sh
@@ -0,0 +1,17 @@
+# This file is to bootstrap the initial management GKE cluster which will be called cluster-00.
+# This using the following documentation https://cloud.google.com/config-connector/docs/how-to/advanced-install
+
+PROJECT_ID=kubecon-mgmt
+CLUSTER_NAME=cluster-00
+SERVICE_ACCOUNT_NAME=kcc-sa
+
+gcloud iam service-accounts create ${SERVICE_ACCOUNT_NAME}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
+  --role="roles/editor"
+
+gcloud iam service-accounts add-iam-policy-binding \
+  ${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
+  --member="serviceAccount:${PROJECT_ID}.svc.id.goog[cnrm-system/cnrm-controller-manager]" \
+  --role="roles/iam.workloadIdentityUser"
