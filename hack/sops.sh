558d79b15ba888ffaff4f7ce93024d23d0fb5ab2
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2023-07-20 21:34:14 +1000

    fix: patchs and update .sops.yaml
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- hack/sops.sh
+++ hack/sops.sh
@@ -1,3 +1,5 @@
+# This file is to bootstrap the SOPs GCP KMS encryption.
+# This using the following documentation https://github.com/getsops/sops#23encrypting-using-gcp-kms
 PROJECT_ID=kubecon-mgmt
 CLUSTER_NAME=cluster-00
 SERVICE_ACCOUNT_NAME=sops-sa
@@ -12,3 +14,7 @@ gcloud iam service-accounts add-iam-policy-binding \
  ${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
   --member="serviceAccount:${PROJECT_ID}.svc.id.goog[flux-system/kustomize-controller]" \
   --role="roles/iam.workloadIdentityUser"
+
+gcloud kms keyrings create sops --location global
+gcloud kms keys create sops-key --location global --keyring sops --purpose encryption
+gcloud kms keys list --location global --keyring sops
