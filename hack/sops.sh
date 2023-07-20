60463f8024a71073793652d3be7bde2cfc3be7fd
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2023-07-20 22:13:21 +1000

    fix: WLI is now working and update cluster alerts
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- hack/sops.sh
+++ hack/sops.sh
@@ -1,5 +1,7 @@
 # This file is to bootstrap the SOPs GCP KMS encryption.
 # This using the following documentation https://github.com/getsops/sops#23encrypting-using-gcp-kms
+# Additional documentation https://fluxcd.io/flux/guides/mozilla-sops/#google-cloud
+
 PROJECT_ID=kubecon-mgmt
 CLUSTER_NAME=cluster-00
 SERVICE_ACCOUNT_NAME=sops-sa
@@ -10,10 +12,16 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
   --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
   --role="roles/editor"
 
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role="roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
 gcloud iam service-accounts add-iam-policy-binding \
  ${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
   --member="serviceAccount:${PROJECT_ID}.svc.id.goog[flux-system/kustomize-controller]" \
   --role="roles/iam.workloadIdentityUser"
+
+
 
 gcloud kms keyrings create sops --location global
 gcloud kms keys create sops-key --location global --keyring sops --purpose encryption
